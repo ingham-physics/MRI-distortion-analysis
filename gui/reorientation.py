55002ea260f7df1af0e4b3a1032b3352e9d28a77
Phillip Chlap <phillip.chlap@unsw.edu.au>
2018-06-26 09:37:58 +1000

    GUI work in progress

---
+++ gui/reorientation.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+try:
+    # Python 2
+    import Tkinter  as tk
+    import ttk
+    import tkMessageBox as messagebox
+    import tkFileDialog as filedialog
+except ImportError:
+    # Python 3
+    import tkinter as tk
+    from tkinter import ttk
+    from tkinter import messagebox
+    from tkinter import filedialog
+
+import os
+
+import logging
+logger = logging.getLogger(__name__)
+
+class ReorientationWindow:
+
+    def __init__(self, parent):
+
+        self.parent = parent
+        self.reoriented_files = []
+
+    def __init__(self, parent):
+
+        self.parent = parent
+
+        self.top = tk.Toplevel(parent)
+        self.top.title('Convert Dicom Files')
+        self.top.geometry('600x340')
+        self.top.update()
+        self.top.minsize(self.top.winfo_width(), self.top.winfo_height())
+        self.top.resizable(False, False)
+        self.top.focus_set()
+        self.top.grab_set()
+
+        self.top.attributes("-topmost", True)
+
+        filesFrame = ttk.Labelframe(self.top, text='File Paths')
+        filesFrame.grid(row=0, padx=5, pady=5, sticky="news")
+
+        self.listbox_paths = tk.Listbox(filesFrame)
+        self.listbox_paths.grid(row=1, columnspan=1, padx=(5,0), pady=5, sticky='news')
+
+        # Add the files from the previous steps
+        try:
+            for f in self.parent.convert_dicom_window.converted_files:
+                self.listbox_paths.insert(tk.END, f)
+        except:
+            # User hasn't run previous step
+            pass
+
+        vsb = ttk.Scrollbar(filesFrame, orient="vertical", command=self.listbox_paths.yview)
+        vsb.grid(row=1, column=2, sticky=("N", "S", "E", "W"), padx=(0,10), pady=(5, 5))
+        self.listbox_paths.configure(yscrollcommand=vsb.set)
+
+        filesFrame.columnconfigure(0, weight=1)
+
+        tk.Button(filesFrame,text='Add File', command=self.add_file, width=20).grid(row=0, padx=5, pady=5)
+        tk.Button(filesFrame,text='Remove Selected', command=self.remove_file, width=20).grid(row=2, padx=5, pady=5)
+        tk.Button(self.top,text='Run Reorientation', command=self.run, width=30, height=2).grid(row=2, padx=5, pady=5)
+
+        self.top.columnconfigure(0, weight=1)
+        self.top.rowconfigure(6, weight=1)
+
+    def add_file(self):
+
+        self.file = os.path.normpath(filedialog.askopenfilename(parent=self.top))
+        self.listbox_paths.insert(tk.END, self.file)
+
+    def remove_file(self):
+
+        selected_indexes = self.listbox_paths.curselection()
+
+        for ind in selected_indexes:
+            self.listbox_paths.delete(int(ind))
+
+    def run(self):
+
+        print(self.listbox_paths.get(0,tk.END))
---
+++ gui/run.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+try:
+    # Python 2
+    import Tkinter  as tk
+    import tkMessageBox as messagebox
+except ImportError:
+    # Python 3
+    import tkinter as tk
+    from tkinter import messagebox
+
+import datetime, logging, sys, os, decimal, subprocess
+
+from convert_dicom import ConvertDicomWindow
+from reorientation import ReorientationWindow
+
+# Log to file and stdout
+log_file_name = 'logs/'+datetime.datetime.today().strftime('%Y')+'/'+datetime.datetime.today().strftime('%m')+'/MRIDA_'+datetime.datetime.today().strftime('%Y-%m-%d_%H_%M_%S')+'.log'
+try:
+    # Python 3
+    os.makedirs(os.path.dirname(log_file_name), exist_ok=True) # > Python 3.2
+except TypeError:
+    # Python 2
+    try:
+        os.makedirs(os.path.dirname(log_file_name))
+    except OSError:
+        if not os.path.isdir(os.path.dirname(log_file_name)):
+            raise
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename=log_file_name,
+                    filemode='w')
+
+# define a new Handler to log to console as well
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console.setFormatter(formatter)
+logger = logging.getLogger('')
+logger.addHandler(console)
+
+logger.info('MRI Distortion Analysis')
+
+class Application(tk.Frame):
+
+    def __init__(self, parent, *args, **kwargs):
+
+        # Setup this window
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        parent.columnconfigure(0, weight=1)
+        parent.rowconfigure(0, weight=1)
+        self.grid(sticky="news")
+
+        self.parent = parent
+
+        tk.Label(self, text="MRI Distortion Analysis", font=("Helvetica", 16)).grid(row=1, padx=5, pady=5)
+
+        tk.Button(self,text='Step 1: Convert DICOM Files', command=self.convert_dicom, width=30, height=2).grid(row=2, padx=5, pady=5)
+        tk.Button(self,text='Step 2: Reorientation', command=self.reorientation, width=30, height=2).grid(row=3, padx=5, pady=5)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(6, weight=1)
+
+        # Prepare Windows
+        self.convert_dicom_window = ConvertDicomWindow(self)
+        self.reorientation_window = ReorientationWindow(self)
+
+    def add_file(self):
+
+        self.directory = os.path.normpath(filedialog.askdirectory(parent=self))
+        self.listbox_paths.insert(tk.END, self.directory)
+
+    def convert_dicom(self):
+        self.convert_dicom_window.show()
+
+    def reorientation(self):
+        self.reorientation_window.show()
+
+# If running main function, launch MainApplication window
+if __name__ == "__main__":
+
+    try:
+        root = tk.Tk()
+        root.title('MRI Distortion Analysis')
+        root.geometry('400x600')
+
+        Application(root).pack(side="top", fill="both", expand=True)
+
+        root.mainloop()
+
+    except Exception as e:
+
+        logging.exception("MRI Distortion Analysis Crash")
+        messagebox.showerror("Error", "An exception has occured and the application must close: " + type(e).__name__)
