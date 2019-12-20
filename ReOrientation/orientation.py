1d6db08b21311742489c9037154b58848c2d1284
Phillip Chlap <phillip.chlap@unsw.edu.au>
2019-12-21 01:05:20 +1100

    Remove a number of milx deps from reorientation

--- ReOrientation/orientation.py
+++ ReOrientation/orientation.py
@@ -3,6 +3,8 @@
 import os
 import subprocess
 
+import SimpleITK as sitk
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -14,40 +16,50 @@ def reorient(input_file, output_path):
     file_base = os.path.join(output_path, filename.split('.')[0])
 
     # Read origin from input_file
-    process = subprocess.Popen(["milxImageInfo", "-o", input_file], stdout=subprocess.PIPE)
-    origin, err = process.communicate()
-    origin = origin.decode("utf-8").replace('\n','').split('x')
+    img = sitk.ReadImage(input_file)
+    origin = img.GetOrigin()
 
     # Change the orientation to look like a conventional scan:
     file_new_origin = file_base + "_m.nii.gz"
-    subprocess.call(["milxImageEditInformation", input_file, file_new_origin, "-m", "-0", "1", "0", "1", "-0", "0", "0", "0", "-1"])
-    
-    # No-orientate the images: 
+    img = sitk.ReadImage(input_file)
+    img.SetDirection((0, 1, 0, 1, 0, 0, 0, 0, -1))
+    sitk.WriteImage(img, file_new_origin)
+
+    # No-orientate the images:
     file_no_orient = file_base + "_nO.nii.gz"
     subprocess.call(["milxImageOperations", "-u", file_new_origin, file_no_orient])
 
     # Re-adjust the origin:
     file_float = file_base + "_float.nii.gz"
-    subprocess.call(["milxImageEditInformation", file_no_orient, file_float, "-o", origin[0], origin[1], origin[2]])
+    img = sitk.ReadImage(file_no_orient)
+    img.SetOrigin(origin)
+    sitk.WriteImage(img, file_float)
 
-    # Multiply the Linac image by 3 to get a better intensity match for generating a mask for the deformation field that incorporates both linac and sim volumes: 
+    # Multiply the Linac image by 3 to get a better intensity match for generating a
+    # mask for the deformation field that incorporates both linac and sim volumes:
     file_x3 = file_base + "x3.nii.gz"
-    subprocess.call(["milxImageOperations", "-C", file_float, "3", file_x3])
+    img = sitk.ReadImage(file_float)
+    img = img * 3
+    sitk.WriteImage(img, file_x3)
 
     # Convert Image to short data type
     file_linac = file_base + "_Linac.nii.gz"
-    subprocess.call(["milxImageConverter", file_float, file_linac, "--componentType", "4"])
+    img = sitk.ReadImage(file_float)
+    img = sitk.Cast(img, sitk.sitkInt16)
+    sitk.WriteImage(img, file_linac)
 
     # Convert x3 image to short data type as well
     file_linac_x3 = file_base + "_Linacx3.nii.gz"
-    subprocess.call(["milxImageConverter", file_x3, file_linac_x3, "--componentType", "4"])
+    img = sitk.ReadImage(file_x3)
+    img = sitk.Cast(img, sitk.sitkInt16)
+    sitk.WriteImage(img, file_linac_x3)
 
     logger.info('Reorientation complete')
 
     # Return the reoriented file
     return file_linac
 
-    
+
 # Runs when this script is executed directly from the commmand line
 if __name__ == "__main__":
 
